44eb08551028613cdb5d62d17b389faee753ae9c
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-08-07 16:48:46 +0530

    Create Number of Substrings Containing All Three Characters.cpp

---
+++ Number of Substrings Containing All Three Characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        vector<int> v(3, -1); int cnt(0);
+
+        for(int i(0);i < s.length();i++){
+            v[s[i] - 'a'] = i;
+            
+            if(v[0] != -1 and v[1] != -1 and v[2] != -1){
+                cnt += 1 + min({v[0], v[1], v[2]});
+            }
+        }
+
+        return cnt;
+    }
+};
