f7d12f2416dace2da74472f13efe4f328fbf5752
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-08-08 14:48:40 +0530

    Create Finding 3-Digit Even Numbers.cpp

---
+++ Finding 3-Digit Even Numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> mp(10, 0);
+        for(auto i:digits) mp[i]++;
+
+        vector<int> ans;
+        for(int i(1);i <= 9;i++){
+            if(mp[i] == 0) continue;
+            mp[i]--;
+
+            for(int j(0);j <= 9;j++){
+                if(mp[j] == 0) continue;
+                mp[j]--;
+
+                for(int k(0);k <= 9;k += 2){
+                    if(mp[k] == 0) continue;
+                    ans.push_back(100 * i + 10 * j + k);
+                }
+
+                mp[j]++;
+            }
+
+            mp[i]++;
+        }
+
+        return ans;
+    }
+};
