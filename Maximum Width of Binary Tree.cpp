ba76dcff2ef9230cae572a36d30c4c760f87fc3e
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-08-13 15:52:06 +0530

    Create Maximum Width of Binary Tree.cpp

---
+++ Maximum Width of Binary Tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root) return 0;
+
+        int ans(0);
+        queue<pair<TreeNode*,int>> q;
+        q.push({root, 0});
+        while(!q.empty()){
+            int size = q.size(), k = q.front().second;
+            int first,last;
+            for(int i(0);i < size;i++){
+                int cur = q.front().second - k;
+                TreeNode* node = q.front().first; q.pop();
+
+                if(i == 0) first = cur;
+                if(i == size - 1) last = cur;
+
+                if(node -> left) q.push({node -> left, (long long)cur * 2 + 1});
+                if(node -> right) q.push({node -> right, (long long)cur * 2 + 2});
+            }
+            ans = max(ans, last - first + 1);
+        }
+
+        return ans;
+    }
+};
