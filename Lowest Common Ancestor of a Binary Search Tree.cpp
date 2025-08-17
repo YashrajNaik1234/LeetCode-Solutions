1420b0240cb26b1843fa2e53f6304f2bed516f22
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-08-17 17:58:34 +0530

    Create Lowest Common Ancestor of a Binary Search Tree.cpp

---
+++ Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr) return root;
+
+        int cur = root -> val;
+        if(cur < p -> val and cur < q -> val) return lowestCommonAncestor(root -> right, p ,q);
+        if(cur > p -> val and cur > q -> val) return lowestCommonAncestor(root -> left, p ,q);
+
+        return root;
+    }
+};
