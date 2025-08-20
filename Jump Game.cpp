c05db14459cac6ab53b6dd0e6f2b7f62caf1867a
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-08-20 19:19:47 +0530

    Create Jump Game.cpp

---
+++ Jump Game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int cnt(0);
+        for(int i(0);i < nums.size();i++){
+            if(i > cnt) return false;
+            cnt = max(cnt, nums[i] + i);
+        }
+
+        return true;
+    }
+};
