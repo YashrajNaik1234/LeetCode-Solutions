d6a07645faf2c5e71a6a9e8d24e7eedbcbc2b8d6
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-09-10 18:05:48 +0530

    Create Course Schedule II.cpp

---
+++ Course Schedule II.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findOrder(int N, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(N);
+        for(int i(0);i < pre.size();i++){
+            adj[pre[i][0]].push_back(pre[i][1]);
+        }
+
+        vector<int> indegree(N, 0);
+        for(int i(0);i < N;i++){
+            for(auto it:adj[i]) indegree[it]++;
+        }
+
+        queue<int> q;
+        for(int i(0);i < N;i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front(); q.pop();
+            topo.push_back(node);
+
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+
+        reverse(topo.begin(),topo.end());
+
+        if(topo.size() == N) return topo;
+        return {};
+    }
+};
