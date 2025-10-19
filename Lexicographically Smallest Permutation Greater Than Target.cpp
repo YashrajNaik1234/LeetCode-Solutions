6074f146b72c60764c9a6fb2818974095e7e1a93
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-10-19 09:09:03 +0530

    Create Lexicographically Smallest Permutation Greater Than Target.cpp

---
+++ Lexicographically Smallest Permutation Greater Than Target.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> cnt;
+    string ans,res; int n;
+    bool dfs(int i, bool x){
+        if(i == n) return x;
+
+        for(int j(0);j < 26;j++){
+            if(cnt[j] == 0) continue;
+            char ch = j + 'a';
+
+            bool s = x or ch > res[i];
+            if(!x and ch < res[i]) continue;
+            cnt[j]--; ans.push_back(ch);
+            if(dfs(i + 1, s)) return true;
+            ans.pop_back(); cnt[j]++;
+        }
+
+        return false;
+    }
+    string lexGreaterPermutation(string s, string target) {
+        res = target; n = s.size(); cnt.assign(26, 0);
+        for(auto i:s) cnt[i - 'a']++;
+        ans = "";
+        if(dfs(0, false)) return ans;
+        return "";
+    }
+};
