7475b94d92e4b88cf3a37f7f87b469c33468b127
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2026-02-28 00:44:26 +0530

    Create Shortest Completing Word.cpp

---
+++ Shortest Completing Word.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string shortestCompletingWord(string licensePlate, vector<string>& words) {
+        int x[26] = {0};
+        for (char ch : licensePlate) if (isalpha(ch)) x[tolower(ch) - 'a']++;
+
+        string result;
+        for (const string& word : words) {
+            int freq[26] = {0};
+            for (char ch : word) ++freq[ch - 'a'];
+
+            bool valid = true;
+            for (int i = 0; i < 26; ++i) {
+                if (x[i] > freq[i]) {
+                    valid = false; break;
+                }
+            }
+            if (valid && (result.empty() || word.length() < result.length())) result = word;
+        }
+        return result;
+    }
+};
