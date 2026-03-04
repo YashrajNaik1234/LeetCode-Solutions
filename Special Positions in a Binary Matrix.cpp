91d4b3da851961f971f8242220e732fe9b134bd1
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2026-03-04 20:00:55 +0530

    Create Special Positions in a Binary Matrix.cpp

---
+++ Special Positions in a Binary Matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<int> rowCount(m, 0);
+        vector<int> colCount(n, 0);
+        
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (mat[row][col] == 1) {
+                    rowCount[row]++; colCount[col]++;
+                }
+            }
+        }
+        
+        int ans = 0;
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (mat[row][col] == 1 and rowCount[row] == 1 and colCount[col] == 1) ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
