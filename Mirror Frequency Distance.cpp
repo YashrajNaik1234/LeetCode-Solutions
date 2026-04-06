fb7393f60af9d8f3e573352c36cb4e2b3edb76c6
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2026-04-06 23:47:47 +0530

    Create Mirror Frequency Distance.cpp

---
+++ Mirror Frequency Distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mirrorFrequency(string s) {
+        vector<int> freq(128, 0);
+        for (char c : s) freq[c]++;
+        
+        int ans = 0;
+        for(char c : s) {
+            if (freq[c] == -1) continue;
+
+            char mirror;
+            if (isdigit(c)) mirror = '9' - (c - '0');
+            else mirror = 'z' - (c - 'a');
+
+            ans += abs(freq[c] - freq[mirror]);
+            freq[c] = -1;
+            freq[mirror] = -1;
+        }
+
+        return ans;
+    }
+};
