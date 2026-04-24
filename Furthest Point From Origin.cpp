d6aefe016020fe0c2940ccfe71e91c85c136900d
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2026-04-24 23:53:08 +0530

    Create Furthest Point From Origin.cpp

---
+++ Furthest Point From Origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int L = 0, R = 0, B = 0;
+        for (auto c : moves) {
+            if (c == 'L') L++;
+            else if (c == 'R') R++;
+            else B++;
+        }
+        
+        return abs(L - R) + B;
+    }
+};
