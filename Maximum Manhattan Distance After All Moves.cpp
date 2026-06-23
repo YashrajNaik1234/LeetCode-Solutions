5d9880c13acf3b9fa15d8ef2baffd85abf834ea2
Yashkingz <132782219+YashrajNaik1234@users.noreply.github.com>
2026-06-24 02:12:53 +0530

    Create Maximum Manhattan Distance After All Moves.cpp

---
+++ Maximum Manhattan Distance After All Moves.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDistance(string moves) {
+        int x = 0, y = 0, count = 0;
+
+        for (auto k : moves) {
+            if (k == 'L') x--;
+            else if (k == 'R') x++;
+            else if (k == 'U') y++;
+            else if (k == 'D') y--;
+            else count++;
+        }
+
+        return abs(x) + abs(y) + count;
+    }
+};
